063bbfd8e5500301add3c188917cf50e80429495
Your Name <you@example.com>
2022-04-04 17:03:57 +0200

    działający wektor

--- Wektor.hh
+++ Wektor.hh
@@ -1,6 +1,10 @@
 #ifndef WEKTOR_HH
 #define WEKTOR_HH
 
+#include <iostream>
+#include <stdlib.h>
+
+
 #define ROZMIAR_STARTOWY 16
 
 using namespace std;
@@ -9,55 +13,66 @@ using namespace std;
 template <typename Typ>
 class Wektor {
 
-    Typ *Tablica;           // tablica dynamicznie alokowana
-    //Typ &referencja;        // referencja do aktualnej tablicy
-    int ind;                // indeks ostatniego zapisanego elementu tablicy
-    unsigned int rozmiar;   // aktualny rozmiar zaalokowanej tablicy
-    
+    Typ *Tablica;  // tablica dynamicznie alokowana
+    int ind;       // indeks ostatniego zapisanego elementu tablicy
+    int rozmiar;   // aktualny rozmiar zaalokowanej tablicy
 
-    public:
-
-    /* Funkcja dodająca element do tablicy, wywołująca realokację
-       pamięci w przypadku braku miejsca */
-    void push_back(Typ)
-    {
-        //
-    }
-    
-    /* Funkcja obsługująca realokację pamięci */
+    // Funkcja obsługująca realokację pamięci 
     void realocate()
     {
         Typ *Nowa_tablica = new Typ[rozmiar*2];
-        for (unsigned int i = 0; i < rozmiar; i++)
+        for (int i = 0; i < rozmiar; i++)
         {
             Nowa_tablica[i] = Tablica[i];
         }
         rozmiar = rozmiar*2;
-        //referencja = &Nowa_tablica;
-        delete Tablica;
-        Tablica = &Nowa_tablica;
+        delete[] Tablica;
+        Tablica = Nowa_tablica;
     }
+
+public:    
+
+    // Funkcja dodająca element do tablicy, wywołująca realokację
+    // pamięci w przypadku braku miejsca 
+    void push_back(Typ nowy_element)
+    {
+        if(ind == rozmiar - 1){
+            realocate();
+        }
+        ind++;
+        Tablica[ind] = nowy_element;
+    }
+
+    // przeciążenie operatora wyłuskania umożliwiające dostęp przez 
+    // referencję do pola Tablica, które jest prywatne
+    Typ & operator[](int indeks) {
+        if ((indeks > rozmiar) || (indeks < 0)) {
+            cout << "Blad. Niepoprawne odwolanie do elementu wektora\n";
+            exit(-1);
+        } else return Tablica[indeks];
+    }
+
+    // przeciążenie operatora wyłuskania tylko do odczytu
+    const Typ & operator[](int indeks) const {
+        if ((indeks > rozmiar) || (indeks < 0)) {
+            cout << "Blad. Niepoprawne odwolanie do elementu wektora\n";
+            exit(-1);
+        } else return Tablica[indeks];
+    } 
 
     // konstruktor klasy Wektor
     Wektor(){
         Tablica = new Typ[ROZMIAR_STARTOWY];
-        ind = new int;
         ind = -1;
-        rozmiar = new unsigned int;
         rozmiar = ROZMIAR_STARTOWY;
-        //referencja = &Tablica;
     }
 
     // destruktor klasy Wektor
     ~Wektor(){
         delete[] Tablica;
-        delete ind;
-        delete rozmiar;
     }
+    
 };
-
-
-
 
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include "Sortowanie.hh"
-#include <iostream>
 
 
 
 int main ()
 {
-    
-   cout << "Hello Wrorld" << ROZMIAR_STARTOWY;
 
+    Wektor<int> wektor1 = Wektor<int>();    
+    cout << "Hello Wrorld" << ROZMIAR_STARTOWY;
+    for (int i = 0; i < 100; i++) { wektor1.push_back(i); cout << wektor1[i] << endl; }
+    
 
     return 0;
 }
